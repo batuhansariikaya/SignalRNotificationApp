79e052236f58738197a6987bce19ee26b859935e
batuhansariikaya <batuhansarikaya008@gmail.com>
2023-07-09 20:52:15 +0300

    jquery kodları yazıldı ve sayfa tasarımı yapıldı

--- SignalRNotificationApp/Program.cs
+++ SignalRNotificationApp/Program.cs
@@ -25,7 +25,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Notfy}/{action=Index}/{id?}");
 app.MapHub<NotifyHub>("/notifyHub");
 
 app.Run();
